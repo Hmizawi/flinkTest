42ab5af009bc10985ed4dc6f89f73f09c5e8f412
Hmizawi <hamza.hachani@supcom.tn>
2018-11-14 10:53:15 +0100

    Update GlobExample.java

--- src/main/java/com/example/flink/GlobExample.java
+++ src/main/java/com/example/flink/GlobExample.java
@@ -11,7 +11,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.util.Arrays;
 import java.util.Collections;
-
+//test
 public class GlobExample {
     public static void main(String... args) throws  Exception {
         File txtFile = new File("/tmp/test/file.txt");
